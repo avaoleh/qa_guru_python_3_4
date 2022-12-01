f85f8ce41466ed1e143767183e78f3e2ff7a7797
AvaOleh <avakimyanov@gmail.com>
2022-12-01 20:52:23 +0300

    add howework_3_4.py

--- homework_3_4.py
+++ homework_3_4.py
@@ -8,22 +8,31 @@ def replace_name_function(name_func, *args):
     print('------------------')
 
 
+def process(my_func, *args):
+
+    #Iterate over all args, convert them to str, and join them
+    args_str = ', '.join(map(str, args))
+
+    #Form the final representation by adding func name
+    print("Name of function: {}; Args: {}".format(my_func.__name__.title().replace("_", " "), ','.join([args_str])))
+
+
 def open_browser(browser_name):
-    replace_name_function(open_browser, browser_name)
+    process(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-    replace_name_function(go_to_companyname_homepage, page_url)
+    process(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    replace_name_function(find_registration_button_on_login_page, page_url, button_text)
+    process(find_registration_button_on_login_page, page_url, button_text)
 
 
 def check_replace_name_function():
-    browser_name = 'Chrome'
-    page_url = 'https://github.com/'
-    button_text = 'Sign up'
+    browser_name = "Chrome"
+    page_url = "https://github.com/"
+    button_text = "login"
 
     open_browser(browser_name)
     go_to_companyname_homepage(page_url)
